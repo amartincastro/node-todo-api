75e31c29d3db9a3fee74c1a8e3e0ea718f4a0528
amartincastro <castroarielm@gmail.com>
2018-11-06 12:36:29 -0800

    update for MongoDB 3.0+

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,11 +1,12 @@
 const MongoClient = require('mongodb').MongoClient;
 
-MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
   if (err) {
     return console.log('Unable to connect to MongoDB server');
   }
 
   console.log('Connected to MongoDB server');
+  const db = client.db('TodoApp') //updates for MongoDB 3.0+
 
   // db.collection('Todos').insertOne({
   //   text: 'Something to do',
@@ -24,11 +25,11 @@ db.collection('Users').insertOne({
   location: 'Reno'
 }, (err, result) => {
   if (err) {
-    return console.log('Unable to insert user', err)
+    return console.log('Unable to insert user', err);
   }
 
   console.log(result.ops);
 });
 
-  db.close();
+  client.close(); //updates for MongoDB 3.0+
 });
