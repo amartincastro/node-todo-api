b8fa8ea9a7245e5f9b3e5c34f3c94509854c6b13
amartincastro <castroarielm@gmail.com>
2018-11-26 12:01:48 -0800

    query db to find user by ID if found, else print error

--- playground/mongoose-queries.js
+++ playground/mongoose-queries.js
@@ -1,23 +1,42 @@
+const {ObjectID} = require ('mongodb');
+
 const {mongoose} = require ('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
+const {User} = require('./../server/models/user')
 
-var id = '6bf3549be7be03901f529006';
 
-Todo.find({
-  _id: id
-}).then((todos) => {
-  console.log('Todos', todos);
-});
 
-Todo.findOne({
-  _id: id
-}).then((todo) => {
-  console.log('Todo', todo);
-});
+// var id = '5bf3549be7be03901f52900611';
+//
+// if (!ObjectID.isValid(id)) {
+//   console.log('ID not valid');
+// }
 
-Todo.findById(id).then((todo) => {
-  if (!todo) {
-    return console.log('Id not found');
+// Todo.find({
+//   _id: id
+// }).then((todos) => {
+//   console.log('Todos', todos);
+// });
+//
+// Todo.findOne({
+//   _id: id
+// }).then((todo) => {
+//   console.log('Todo', todo);
+// });
+
+// Todo.findById(id).then((todo) => {
+//   if (!todo) {
+//     return console.log('Id not found');
+//   }
+//   console.log('Todo By Id', todo);
+// }).catch((e) => console.log(e));
+
+// User.findByID, 3 cases: query works but uer not found, user found then print, and any errors
+User.findById('5beb05eb5504fb4c27befe1d').then((user) => {
+  if (!user) {
+    return console.log('Unable to find user');
   }
-  console.log('Todo By Id', todo);
-});
+  console.log(JSON.stringify(user, undefined, 2));
+}, (e) => {
+  console.log(e);
+})
