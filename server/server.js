f4c55460f07f0b8ec8d67c4dc112a5e135613e6f
amartincastro <castroarielm@gmail.com>
2018-11-26 14:51:12 -0800

    setup query on db using URL params

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //library imports
 var express = require('express');
 var bodyParser = require('body-parser');
+var {ObjectID} = require('mongodb');
 
 //local imports
 var {mongoose} = require ('./db/mongoose');
@@ -29,6 +30,35 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+//GET /todos/123
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+if (!ObjectID.isValid(id)) {
+  return res.status(404).send();
+}
+
+Todo.findById(id).then((todo) => {
+  if (!todo) {
+    return res.status(404).send();
+  }
+
+  res.send({todo});
+
+}).catch((e) => {
+  res.status(400).send();
+});
+  //validate ID as valid using isValid
+    // else respond 404 - send back empty body using send without passing in a value     res.status(400).send(e);
+
+//then queryt database looking for matching document using findById
+  // success
+    // if todo success - send it back
+    // if no todo - send 404 with empty body
+    // 400 - send empty body
+
 });
 
 app.listen(3000, () => {
