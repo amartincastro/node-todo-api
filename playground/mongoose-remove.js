8239a433320b697424995b8b93657d462b901006
amartincastro <castroarielm@gmail.com>
2018-11-29 14:45:52 -0800

    remove Todo by ID

--- playground/mongoose-remove.js
+++ playground/mongoose-remove.js
@@ -3,3 +3,14 @@ const {ObjectID} = require ('mongodb');
 const {mongoose} = require ('./../server/db/mongoose');
 const {Todo} = require('./../server/models/todo');
 const {User} = require('./../server/models/user')
+
+// Todo.remove({}).then((result) => {
+//   console.log(result);
+// });
+
+// Todo.findOneAndRemove
+// Todo.findByIdAndRemove
+
+Todo.findByIdAndRemove('5c006bd2f984c0d6d1abf5cc').then((todo) => {
+  console.log(todo);
+});
