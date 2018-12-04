a824508012d14a1bb56dc59768f42bfe8504b145
amartincastro <castroarielm@gmail.com>
2018-12-04 10:34:28 -0800

    test and validation for PATCH /todos/:id updates to text, completed value, and completedAt time

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -144,6 +144,24 @@ describe('DELETE /todos/:id', () => {
 
 describe('PATCH /todos/:id', () => {
   it('should update the todo', (done) => {
+    var hexId = todos[0]._id.toHexString();
+    var text = 'This should be the new text';
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        completed: true,
+        text
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done)
+
+
     //grab id of first item,
     // update text, set completed true
     // assert you get 200 back
